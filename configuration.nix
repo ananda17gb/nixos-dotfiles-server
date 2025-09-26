80986cc9305f42648635dcc3e1c430012ffd3ab7
ananda17gb <ananda.widigdo17@gmail.com>
2025-09-26 21:13:27 +0700

    cleaning configuration.nix, replace copyparty with filebrowser

--- configuration.nix
+++ configuration.nix
@@ -1,64 +1,22 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos-server"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
   time.timeZone = "Asia/Jakarta";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ananda = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "noodledrive" ]; 
     packages = with pkgs; [
       tree
     ];
@@ -68,11 +26,8 @@
     shell = pkgs.zsh;
   };
 
-  # programs.firefox.enable = true;
   programs.zsh.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     neovim 
     lazygit
@@ -83,6 +38,7 @@
     adguardhome
     ntfs3g
     tailscale
+    filebrowser
   ];
 
  services.adguardhome = {
@@ -93,55 +49,44 @@
   };
 
   services.tailscale.enable = true;
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 3000 80 3210 ];
+  networking.firewall.enable = true; 
+  networking.firewall.allowedTCPPorts = [ 3000 80 3030 ];
   networking.firewall.allowedUDPPorts = [ 53 4164 ];
-
   services.resolved.enable = false;
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  users.users.noodledrive.isSystemUser = true;
+  users.users.noodledrive.group = "noodledrive";
+  users.groups.noodledrive = { };
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  systemd.tmpfiles.rules = [
+    "d /var/lib/noodledrive 0770 noodledrive noodledrive"
+  ];
+
+  # 4. Define the systemd service (MODIFIED: ExecStart paths changed)
+  systemd.services.noodledrive = {
+    after = [ "network.target" "mnt-harddisk.mount"];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      User = "noodledrive";
+      Restart = "on-failure";
+      # The ExecStart command now points to the new paths.
+      ExecStart = ''
+        ${pkgs.filebrowser}/bin/filebrowser \
+	  --address 0.0.0.0 \
+          --port 3030 \
+          --database /var/lib/noodledrive/filebrowser.db \
+          --root /mnt/harddisk
+      '';
+    };
+  };
+
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
 
--- flake.nix
+++ flake.nix
@@ -6,10 +6,9 @@
 			url = "github:nix-community/home-manager/release-25.05";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
-		copyparty.url = "github:9001/copyparty";
 	};
 	
-	outputs = { self, nixpkgs, home-manager, copyparty, ... }: {
+	outputs = { self, nixpkgs, home-manager, ... }: {
 		nixosConfigurations.nixos-server = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 			modules = [
@@ -23,43 +22,6 @@
 						backupFileExtension = "backup";
 					};
 				}	
-				copyparty.nixosModules.default
-				({ pkgs, ... }: {
-				  # add the copyparty overlay to expose the package to the module
-				  nixpkgs.overlays = [ copyparty.overlays.default ];
-				  # (optional) install the package globally
-				  environment.systemPackages = [ pkgs.copyparty ];
-				  # configure the copyparty module
-				  services.copyparty = {
-				    enable = true;
-
-				    settings = {
-				      i = "0.0.0.0";
-				      p = [ 3210 ];
-				    };
-
-				    accounts = {};
-
-				    volumes = {
-				      "/harddisk" = {
-				        path = "/mnt/harddisk";
-
-				      access = {
-				        rw = "*";
-				      };
-
-				      flags = {
-				        scan = 60;
-					e2d = true;
-				        fk = 4;
-				        d2t = true;
-				      };
-				    };
-				      };
-
-				    openFilesLimit = 8192;
-				  };
-				})
 			];
 		};
 	};
